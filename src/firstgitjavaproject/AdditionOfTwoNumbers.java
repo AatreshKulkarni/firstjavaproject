8b20c5872f1f41f11f0c0deb92166e9f10287615
Prasad Kulkarni <aatresh.kulkarni08@gmail.com>
2017-12-04 23:41:15 +0530

    Program to add 2 numbers

---
+++ src/firstgitjavaproject/AdditionOfTwoNumbers.java
@@ -0,0 +1,11 @@
+package firstgitjavaproject;
+
+public class AdditionOfTwoNumbers {
+
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 20;
+		System.out.println("Addtion of x and y is " + (x + y));
+	}
+
+}
